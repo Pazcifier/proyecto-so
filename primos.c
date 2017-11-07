fb3347c2cb9788a23fabfb97be0e031a259ddf7c
Pazcifier <jpaz17@hotmail.com>
2017-11-06 21:00:02 -0400

    Pepa pero desordenado

--- primos.c
+++ primos.c
@@ -5,10 +5,18 @@
 #include <time.h>
 #include <pthread.h>
 
+struct hilos_primos {
+  int lineas;
+  int nroHilo;
+  int inicio;
+  FILE *archivo;
+};
+
 int VerificarPrimo(int num);
 int numLineas(FILE *fp);
 int lineaToInt(FILE *fp);
-void *salidaPrimo ( int lineasT, int i, FILE *fp);
+void salidaPrimo ( int lineasT, int i, FILE *fp);
+void *salidaPrimoHilos (void* arg);
 void PosicionArchivo(FILE *fp);
 
 int main(int argc, char const *argv[]) {
@@ -16,7 +24,9 @@ int main(int argc, char const *argv[]) {
   FILE *fp; //Variable para leer el archivo
   int n = atoi(argv[4]); //Cantidad de procesos/hilos trabajadores
   pthread_t hilos[n]; //Vector de hilos trabajadores
+  struct hilos_primos hp[n];
   int cont = 0; //Cantidad de líneas en el archivo
+  int lineasInicio = 0;
   int lineasTrabajador = 0; //Lineas asignadas al trabajador
   int lineasTrabajadorUltimo = 0; //Lineas asignadas al maestro
   clock_t comienzo, fin; //Variables para tomar tiempo de ejecución
@@ -45,15 +55,24 @@ int main(int argc, char const *argv[]) {
         }
       } else if (strcmp(argv[2], "-h") == 0) {
         for(int i = 0; i < n; i++) {
+          hp[i].archivo = fp;
+          hp[i].inicio = lineasInicio;
+          hp[i].nroHilo = i;
+          printf("%d\n", hp[i].inicio);
           printf("Hilo %d\n", i);
-          //int *apContadorFor = i;
           if (i == (n-1)) {
-            pthread_create(&hilos[i], NULL, salidaPrimo(lineasTrabajadorUltimo, i, fp), NULL);
+            hp[i].lineas = lineasTrabajadorUltimo;
+            pthread_create(&hilos[i], NULL, salidaPrimoHilos, &hp[i]);
           } else {
-            pthread_create(&hilos[i], NULL, salidaPrimo(lineasTrabajador, i, fp), NULL);
+            hp[i].lineas = lineasTrabajador;
+            pthread_create(&hilos[i], NULL, salidaPrimoHilos, &hp[i]);
           }
+          printf("Sale de salidaPrimo\n");
+          lineasInicio += lineasTrabajador;
         }
+
        for(int i = 0; i < n; i++) {
+          printf("Esperando hilo %d\n", i);
           pthread_join(hilos[i], NULL);
         }
       }
@@ -125,7 +144,7 @@ int lineaToInt(FILE *fp){
     return num;
 }
 
-void *salidaPrimo ( int lineasT, int i, FILE *fp){
+void salidaPrimo ( int lineasT, int i, FILE *fp){
     int lineaInt;
     FILE *salida;
     char texto[10];
@@ -144,5 +163,30 @@ void *salidaPrimo ( int lineasT, int i, FILE *fp){
 
     }
     fclose(salida);
+    //fclose(fp);
+}
+
+void *salidaPrimoHilos (void* arg){
+    struct hilos_primos *hp = (struct hilos_primos *) arg;
+
+    int lineaInt;
+    FILE *salida;
+    char texto[10];
+    char lineaS[20];
+    sprintf(texto, "%d.txt", hp->nroHilo);
+    salida = fopen(texto,"w");
+ 	//fp= fopen(argv[1],"r");
+ 	//PosicionArchivo(fp,contLast);
+
+    for (int j = 0;j< hp->lineas; j++){
+      lineaInt=lineaToInt(hp->archivo);
+      sprintf(lineaS,"%d %d",lineaInt,VerificarPrimo(lineaInt));
+      //printf("%s \n",lineaS);
+      //fprintf(salida, lineaS);
+      fprintf(salida, "%s \n",lineaS);
+
+    }
+    fclose(salida);
+    pthread_exit(0);
     //fclose(fp);
 }
